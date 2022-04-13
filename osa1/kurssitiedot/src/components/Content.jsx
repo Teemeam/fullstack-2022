463e80bfcd3964959ca21f686715ec9d9122a7d2
Teemeam <teemu.kakko@yle.fi>
2022-04-13 14:39:38 +0300

    Finish exercises 1.1.-1.2.

---
+++ osa1/kurssitiedot/src/components/Content.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Part } from './Part.jsx';
+
+export const Content = (props) => {
+  return (<div>
+    <Part part={ props.part1 } exercises={ props.exercises1 }/>
+    <Part part={ props.part2 } exercises={ props.exercises2 }/>
+    <Part part={ props.part3 } exercises={ props.exercises3 }/>
+  </div>);
+};
---
+++ osa1/kurssitiedot/src/components/Part.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Part = (props) => {
+  return (<p>{ props.part } { props.exercises }</p>);
+};
